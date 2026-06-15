c7c720ccf428a418d0d4978c5372ebaff56cddac
SAGINOYA\saginoya <saginoya@create-nippo.co.jp>
2026-06-15 16:03:19 +0900

    feat: 型定義ファイル更新

--- env.d.ts
+++ env.d.ts
@@ -1,1 +1,18 @@
 /// <reference types="vite/client" />
+
+declare module '@splidejs/vue-splide' {
+  import type { App, Component } from 'vue'
+
+  export interface Options {
+    [key: string]: unknown
+  }
+
+  export const Splide: Component
+  export const SplideSlide: Component
+  export const SplideTrack: Component
+  export const VueSplide: { install(app: App): void }
+
+  export default VueSplide
+}
+
+declare module '@splidejs/vue-splide/css'
